d0a680ff2a0b7a818bd50677fc06474fe71cc8c9
Bruno Vinícius <vbruno96@gmail.com>
2025-02-20 15:44:51 -0300

    build: install and configure zod

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,49 @@
-import {fastify} from "fastify"
-import {fastifyCors} from '@fastify/cors'
-const app = fastify()
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.register(fastifyCors, {
-  origin: true
+  origin: true,
 })
 
-app.get('/hello', () => {
-  return {
-    message: 'Hello World'
+app.post(
+  '/subscriptions',
+  {
+    schema: {
+      body: z.object({
+        fullname: z.string().min(3, 'Digite o nome completo'),
+        email: z.string().email('Digite um email válido'),
+      }),
+      response: {
+        201: z.object({
+          fullname: z.string(),
+        }),
+      },
+    },
+  },
+  async (request, reply) => {
+    const { fullname, email } = request.body
+
+    return reply.status(201).send({
+      fullname,
+    })
   }
-})
+)
 
-app.listen({
-  port: 6969
-}).then(() => {
-  console.log('🚀 Server is running')
-})
+app
+  .listen({
+    port: 6969,
+  })
+  .then(() => {
+    console.log('🚀 Server is running')
+  })
